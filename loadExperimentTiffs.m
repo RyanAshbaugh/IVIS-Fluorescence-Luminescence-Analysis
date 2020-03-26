a08d6a9307f71e6fa9cfaaa5aaf1612df89aa4d7
Ryan Ashbaugh <ashbau12@msu.edu>
2020-03-26 12:29:50 -0400

    add comments

--- loadExperimentTiffs.m
+++ loadExperimentTiffs.m
@@ -1,7 +1,14 @@
 function [ photos_cell, lumis_cell ] = loadExperimentTiffs( ...
     photo_struct, lumi_struct, num_reads )
-%loadExperimentTiffs Summary of this function goes here
-%   Detailed explanation goes here
+%loadExperimentTiffs load all of the images for selected experiment
+%   Arguments:
+%		photo_struct - struct populated with info about experiment and image
+%			file names. Has the folder names as well.
+%		lumi_struct - same as photo_struct, but with the luminescence images
+%		num_reads - number of measurements/images
+%	Returns:
+%		photos_cell - num_reads x 1, cell array with all images
+%		lumis_cell - num_reads x 1, cell array with all luminescence images
 
     % load each photo and luminescence image 
     photos_cell = cell( num_reads, 1 );
