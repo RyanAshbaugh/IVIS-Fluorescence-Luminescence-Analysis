c0db6b17383b4a4daf0de9c640afd1388693ee5d
Ryan Ashbaugh <ashbau12@msu.edu>
2020-04-05 21:22:57 -0400

    visibility changes for final well centers

--- displayAnalysisPhoto.m
+++ displayAnalysisPhoto.m
@@ -1,18 +1,19 @@
 function image_handle = displayAnalysisPhoto( photo_histeq, image_scale, ...
-    final_well_centers, final_well_radii )
+    final_well_centers, final_well_radii, visible )
 %	displayAnalysisPhoto display an image of one photo with all detected wells
 %	Arguments:
 %		photo_histeq - size of input photo, histogram equalized input image
 %		image_scale - how much to scale up size of image by
 % 		final_well_centers - num_wells x 2, sorted x y well center locations
 %		final_well_radii - num_wells x 1, sorted radii in pixels
+%		visible - on or off to show image
 %	Returns:
 %		image_handle - handle to image/figure
 
     num_wells = size( final_well_radii, 1 );
 
     % show the image, but resize it to make it bigger
-    figure;
+    figure( 'visible', visible );
     image_handle = imshow( ...
         imresize(photo_histeq, image_scale * size( photo_histeq ) ) );
     hold on;
