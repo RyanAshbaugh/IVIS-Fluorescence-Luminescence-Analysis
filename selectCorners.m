ee4c3fb99db2d64aa3ed792711ffe48c146f2863
Ryan Ashbaugh <ashbau12@msu.edu>
2020-05-05 10:44:58 -0400

    changed output variables to 1

--- selectCorners.m
+++ selectCorners.m
@@ -1,7 +1,7 @@
-function [ corners_positions, corners_handles ] = selectCorners()
+function corners_positions = selectCorners()
 
 	% pick points
-	disp('Select four corner wells');
+	%disp('Select four corner wells');
 	empty_point = images.roi.Point;
 	corners_handles = [ empty_point empty_point empty_point empty_point ];
 	corners_positions = zeros( 4, 2 );
@@ -10,7 +10,8 @@ function [ corners_positions, corners_handles ] = selectCorners()
 	for ii = 1:4
 
 		% draw the point and then add positions to position array
-		corners_handles( ii ) = drawpoint( 'Color', 'r', 'LineWidth', 1 );
+		corners_handles( ii ) = ...
+			drawpoint( 'Color', 'r', 'LineWidth', 1 );
 		corners_positions( ii, : ) = corners_handles( ii ).Position;
 
 	end
