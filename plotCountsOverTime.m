63a35d42fc4ec85909704cac9149da6caf80044b
Ryan Ashbaugh <ashbau12@msu.edu>
2020-05-18 19:52:33 -0400

    only plot if not empty

--- plotCountsOverTime.m
+++ plotCountsOverTime.m
@@ -10,10 +10,14 @@ function msg = plotCountsOverTime( image_axes, well_counts, ...
 		return
 	else
 		hold( image_axes, 'on' );
+
+		num_readings = size( normalized_counts, 2 );
+		readings_vector = 0:( num_readings - 1 );
 		for ii = 1:size( groups, 2 )
 			group_counts = normalized_counts( groups{ii}{2}, : );
-
-			plot( image_axes, group_counts );
+			if ~isempty( group_counts )
+				plot( image_axes, readings_vector, group_counts );
+			end
 
 		end
 		hold( image_axes, 'off' );
