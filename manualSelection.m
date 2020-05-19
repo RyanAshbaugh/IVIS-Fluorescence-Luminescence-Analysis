f95ef7183f7203c08eb049a25636ad1045c1ca36
Ryan Ashbaugh <ashbau12@msu.edu>
2020-05-19 10:39:01 -0400

    transposed to a temp variable to make sure it sorts left right, top bottom

--- manualSelection.m
+++ manualSelection.m
@@ -7,10 +7,13 @@ function all_centers_mat = manualSelection( num_rows, num_cols )
 	final_corner_positions = sortSelectedCorners( corner_array );
 
 	% m x n x 2 mat with the last dimension for X and Y positions
-	all_wells_tensor = generateAllWells( final_corner_positions, num_cols, num_rows );
+	all_wells_tensor = generateAllWells( final_corner_positions, num_cols, num_rows )
 
 	% reshape the array to be m*n x 2
-	all_centers_mat = reshape( all_wells_tensor, num_cols * num_rows, 2, 1 );
+	% first transpose to temp variable so that it sorts left to right, up down
+	tmp(:,:,1) = all_wells_tensor(:,:,1)';
+	tmp(:,:,2) = all_wells_tensor(:,:,2)';
+	all_centers_mat = reshape( tmp, num_cols * num_rows, 2, 1 )
 
 end
 
