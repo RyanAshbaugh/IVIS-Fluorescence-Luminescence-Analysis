a619915c0c2cf51a51c848f2eb4df0b55075fd90
Ryan Ashbaugh <ashbau12@msu.edu>
2020-05-18 21:52:26 -0400

    added mean of group functionality

--- plotCountsOverTime.m
+++ plotCountsOverTime.m
@@ -1,6 +1,5 @@
-function msg = plotCountsOverTime( image_axes, well_counts, ...
-		normalized_counts, groups )
-
+function msg = plotCountsOverTime( image_axes, normalized_counts, groups, ...
+		mean_flag )
 	
 	if isempty( normalized_counts )
 		msg = 'No well count data to plot';
@@ -9,17 +8,36 @@ function msg = plotCountsOverTime( image_axes, well_counts, ...
 		msg = 'No well groups found. Generate wells and groups';
 		return
 	else
+		cla( image_axes );
+
 		hold( image_axes, 'on' );
 
 		num_readings = size( normalized_counts, 2 );
 		readings_vector = 0:( num_readings - 1 );
-		for ii = 1:size( groups, 2 )
-			group_counts = normalized_counts( groups{ii}{2}, : );
-			if ~isempty( group_counts )
-				plot( image_axes, readings_vector, group_counts );
-			end
+		title_str = 'Well counts for each measurement';
 
+		num_groups = size( groups, 2 );
+		colors = hsv( num_groups );
+		legend_names = {};
+		for ii = 1:num_groups
+			group_counts = normalized_counts( groups{ii}{2}, : );
+			legend_names{ ii } = groups{ ii }{ 1 };
+			if ~isempty( group_counts )
+				if mean_flag
+					plot( image_axes, readings_vector, mean( group_counts ), ...
+						'Color', colors(ii,:));
+					title_str = 'Avg. group well counts for each measurement';
+				else
+					plot( image_axes, readings_vector, group_counts, ...
+						'Color', colors(ii,:));
+				end
+			end
 		end
+		legend( image_axes, legend_names );
+		title( image_axes, title_str );
+		xlabel( image_axes, 'Measurement number' );
+		ylabel( image_axes, 'Relative Luminescence Units' );
+
 		hold( image_axes, 'off' );
 		msg = 'Plotted normalized well counts for each group';
 	end
